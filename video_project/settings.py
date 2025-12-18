c2535694a89b92802c713578f750595c87576938
Muhammed Adnan K <73103641+muhammedadnank@users.noreply.github.com>
2025-12-18 19:44:33 +0530

    Change database settings to use environment variable
    Updated database configuration to use DATABASE_URL environment variable and added SSL requirement.

--- video_project/settings.py
+++ video_project/settings.py
@@ -81,8 +81,9 @@ WSGI_APPLICATION = 'video_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
